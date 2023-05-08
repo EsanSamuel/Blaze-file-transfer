8ed646f5c2457cf14acbcff70e4d04445557a3c6
EsanSamuel <118141696+EsanSamuel@users.noreply.github.com>
2023-05-08 21:28:39 +0100

    Update Help.jsx

--- src/Pages/Help.jsx
+++ src/Pages/Help.jsx
@@ -17,7 +17,7 @@ const Help = () => {
       </div>
 
       <div className='flex justify-center items-center bg-none'>
-        <MdSupportAgent className='text-[170px] text-[#0d6efd] sm:mt-[50%] mt-[120%] bg-none absolute'/>
+        <MdSupportAgent className='text-[170px] text-[#0d6efd] sm:mt-[50%] mt-[100%] bg-none absolute'/>
       </div>
 
 
