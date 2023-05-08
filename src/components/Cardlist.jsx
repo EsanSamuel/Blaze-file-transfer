607a24684adcc5d037262524be9aaa321a8c84f1
EsanSamuel <118141696+EsanSamuel@users.noreply.github.com>
2023-05-08 23:08:55 +0100

    Update Cardlist.jsx

--- src/components/Cardlist.jsx
+++ src/components/Cardlist.jsx
@@ -4,10 +4,10 @@ import { VscEmptyWindow } from 'react-icons/vsc'
 
 const Cardlist = ({ card, handleAddCard, handleDeleteCard }) => {
     return (
-        <div className='sm:p-20 px-5 pb-20 gap-5 sm:mt-0 mt-5 bg-none absolute'>
+        <div className='sm:p-20 px-5 pb-20 gap-5 sm:mt-0 mt-5 bg-none'>
             <h1 className='font-bold sm:hidden'>Recent rooms</h1>
             {card?.length > 0 ? (
-                <div className='gap-1 sm:gap-2 grid grid-cols-1 sm:grid-cols-3'>
+                <div className='gap-1 sm:gap-2 grid grid-cols-1 sm:grid-cols-3 absolute'>
                     {card.reverse().map((cardx) => (
                         <Card
                             id={cardx.id}
