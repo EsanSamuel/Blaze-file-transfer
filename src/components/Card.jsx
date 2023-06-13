e24260863f8c27841580aaf038416b8190677309
Esan Samuel <esansamuel555@gmail.com>
2023-06-13 01:46:06 +0100

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import { AiOutlineClose } from 'react-icons/ai'
 const Card = ({ id, text, date ,handleDeleteCard}) => {
   return (
     <div className='sm:p-20 px-10 py-3  border-2 border-[#3a3a43] w-full rounded hover:border-[#1dc071] mt-3'>
-      <Link to='/share'><h1 className='sm:text-[20px] text-[17px]'>{text}</h1>
+      <Link to={`/share/${text}`}><h1 className='sm:text-[20px] text-[17px]'>{text}</h1>
       <div className='bottom-0 mb-2'>
         <small className='mt-20 text-[#5f5f5f]'>{date}</small>
         
